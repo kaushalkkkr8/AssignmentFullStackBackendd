9d99b03fb963ea5314aafbb9b61434c0ab114e6a
Kaushal <139836289+kaushalkkkr8@users.noreply.github.com>
2024-12-17 19:48:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ require("dotenv").config()
 require("./db.connect")
 
 app.use(bodyParser.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: 'GET,POST,PUT,DELETE', 
+  credentials: true, 
+}));
 
 
 app.use('/auth',authRouter)
