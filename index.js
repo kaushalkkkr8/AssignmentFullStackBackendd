4d08054566567f245e1afec5b6c6c7a4d965279d
Kaushal <kaushalkr8@gmail.com>
2024-12-17 20:31:51 +0530

    modefied index

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ require("dotenv").config();
 require("./db.connect");
 
 
+
 app.use(bodyParser.json());
-app.use(cors());
-
-app.use(bodyParser.json())
 // app.use(cors())
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  methods: 'GET,POST,PUT,DELETE', 
-  credentials: true, 
-}));
-
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 app.use("/auth", authRouter);
 app.use("/auth", authProfile);
