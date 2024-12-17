1fd8dffe3da16c0173692ce5595b4b5a79f4b6e7
Kaushal <kaushalkr8@gmail.com>
2024-12-17 20:22:22 +0530

    Merge remote-tracking branch 'origin/main'

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require("express")
-const app= express()
-const cors=require("cors")
-const bodyParser= require("body-parser")
-const authRouter=require("./Routes/authRoute")
-const authProfile = require("./Routes/authProfiles")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const authRouter = require("./Routes/authRoute");
+const authProfile = require("./Routes/authProfiles");
 
-require("dotenv").config()
-require("./db.connect")
+require("dotenv").config();
+require("./db.connect");
 
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
+app.use("/auth", authRouter);
+app.use("/auth", authProfile);
 
-app.use('/auth',authRouter)
-app.use('/auth',authProfile)
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
-const PORT=4000
-app.listen(PORT,()=>{
-    console.log("app is running on port ",PORT);
-    
-})
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log("app is running on port ", PORT);
+});
